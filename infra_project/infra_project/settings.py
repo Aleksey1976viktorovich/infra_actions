2235ef6e0eba0b5b1570d8268b71c1dbaf0d308e
aleksey1976viktorovich <aleksey1976viktorovich@mail.ru>
2022-05-11 15:29:49 +1000

    flake8 test1.2

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'infra_app.apps.InfraAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
